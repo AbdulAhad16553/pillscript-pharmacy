06b746d872413d1418a704898db6c37587a967bf
Khalil ahmed <82217781+khalil72@users.noreply.github.com>
2026-01-07 23:50:13 +0500

    distributor add

--- modules/auth/distributors/distributor-card/index.tsx
+++ modules/auth/distributors/distributor-card/index.tsx
@@ -1,11 +1,22 @@
+import { Copy, Check } from "lucide-react";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 const DistributorCard = ({ dist }: any) => {
+    const [copiedIndex, setCopiedIndex] = useState<number | null>(null);
+
+  const handleCopy = (number: string, idx: number) => {
+    navigator.clipboard.writeText(number);
+    setCopiedIndex(idx);
+
+    setTimeout(() => {
+      setCopiedIndex(null);
+    }, 2000);
+  };
   return (
     <div
       key={dist.id}
-      className="rounded-xl border bg-white p-4 shadow-sm hover:shadow-md transition h-full max-h-60 md:max-h-44 flex flex-col"
+      className="rounded-[10px] border border-[#dfe3e6] bg-white p-4 w-full  cursor-pointer transition h-full  flex flex-col"
     >
       <div className="flex items-start gap-4">
        
@@ -20,20 +31,38 @@ const DistributorCard = ({ dist }: any) => {
        
         <div className="flex-1 flex flex-col justify-between">
           <div>
-            <h2 className="font-semibold text-sm line-clamp-1">
+            <h2 className="font-semibold text-[15px] md:text-[17px] lg:text-[20px] ">
               {dist.name} – {dist.district}
             </h2>
-            <p className="text-xs text-gray-500">{dist.type}</p>
+            <p className="text-14px] text-[#8897a2]">{dist.type}</p>
           </div>
 
          
-          <div className="text-sm text-black mt-1 space-y-0.5 max-h-16 overflow-auto">
-            {dist.phone.map((p: string, idx: number) => (
-              <p key={idx} className="truncate">
-               {p}
-              </p>
-            ))}
+      <div className="text-sm text-black mt-1 space-y-1 max-h-16 overflow-auto">
+      {dist.phone.map((p: string, idx: number) => (
+        <div key={idx}>
+          <div
+            onClick={() => handleCopy(p, idx)}
+            className="flex items-center gap-2 cursor-pointer"
+          >
+            <p className="truncate">{p}</p>
+
+            {copiedIndex === idx ? (
+              <Check size={14} className="text-blue-500" />
+            ) : (
+              <Copy size={14} className="text-blue-500" />
+            )}
           </div>
+
+          {copiedIndex === idx && (
+            <p className="text-xs text-blue-500 mt-0.5">
+              Contact has been copied
+            </p>
+          )}
+        </div>
+      ))}
+    </div>
+          
 
         
           <p className="text-xs text-gray-500 mt-1 line-clamp-2">
